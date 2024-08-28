6f1520a610a5cd851af08e63c6bcda12c3494ba5
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2024-08-28 09:42:13 +0000

    add cron

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import screenshot from "./src/screenshot.mjs";
+import cron from "node-cron";
 
-screenshot();
+//
+cron.schedule("* * * * *", async () => {
+  for (let index = 0; index < 5; index++) {
+    console.log("cron started", index + 1);
+    await screenshot("./bucket", "screenshot");
+    console.log("cron end");
+  }
+});
--- src/screenshot.mjs
+++ src/screenshot.mjs
@@ -1,22 +1,25 @@
 import puppeteer from "puppeteer";
 
 const screenshot = async (path, filename) => {
+  const timestamp = Date.now();
+  const location = `${path}/${filename}_${timestamp}.png`
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Masukkan URL yang ingin di-screenshot
-  await page.goto("https://www.instagram.com/aulia.tazz/", {
+  await page.goto("https://microsoft.com", {
     waitUntil: "networkidle2",
   });
 
   await page.waitForNetworkIdle();
 
   // Screenshot dan simpan ke file
-  await page.screenshot({ path: "screenshot.png", fullPage: true });
+  await page.screenshot({ path:location, fullPage: true });
 
   await browser.close();
   console.log(
-    "Screenshot berhasil diambil dan disimpan sebagai screenshot.png",
+    `Screenshot berhasil diambil dan disimpan di ${location}`
   );
 };
 
